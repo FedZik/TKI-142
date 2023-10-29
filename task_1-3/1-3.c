4de964181e2166549c79318ed25011cdac73e235
FedZik <kostikshabin2005@gmail.com>
2023-10-29 22:54:39 +0300

    create 1.3

---
+++ task_1-3/1-3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* @brief Поиск значения energy
+* @param  "m" масса для функции energy
+* @param  "v" скоркость для функции energy
+* @return Возвращает результат функции getEnergy
+*/
+double getEnergy(double m, double v);
+
+/**
+* @brief Точка входа в программу
+* @return Возвращает 0, если программа работает корректно, иначе 1
+*/
+int main()
+{
+    double m, v;
+    scanf("%lf", &m);
+    scanf("%lf", &v);
+    if (m > 0 && v > 0 && m != 1 && v != 1)
+    {
+        double energy = getEnergy(m, v);
+        printf("\n energy=%lf", energy);
+    }
+    else
+    {
+        printf("\n %s", "Неправильный ввод");
+    }
+    return 0;
+}
+
+double getEnergy(double m, double v)
+{
+    return m*pow(v,2)/2;
+}
