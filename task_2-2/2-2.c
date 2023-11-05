d3ef0d540602c10b686e55c3817e6d5562717667
FedZik <kostikshabin2005@gmail.com>
2023-11-06 02:12:24 +0300

    Create 2-2.c

---
+++ task_2-2/2-2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+/**
+* @brief расчет y
+* @param x значение х 
+* @param a значение а
+* @return возвращает значение у
+**/
+double yFunc(double x, const double a);
+
+/**
+* @brief проверка введеного значения
+* @return возвращает значение если ввод правильный, иначе выводит сообщение об ошибке
+*/
+float getNumber();
+
+/**
+ * @brief точка входа в программу 
+ * @return возвращает 0, если все верно, возвращает 1 в случае ошибки
+ **/
+int main()
+{
+    const double a = 2.5;
+    double x;
+
+    x = getNumber();
+
+    double y = yFunc(x, a);
+
+    printf("\n y= %lf", y);
+    
+    return 0;
+}
+
+float getNumber() 
+{ 
+ float number; 
+ if (scanf("%f", &number) == 1) 
+ { 
+  return number; 
+ } 
+ printf("\n" "Неправильный ввод"); 
+ abort(); 
+} 
+
+double yFunc(double x, const double a)
+{
+    if (x <= a){ return x * sin(a) * x; };
+    if (x > a){ return x * pow(x - a, 1.0 / 3); };
+}
