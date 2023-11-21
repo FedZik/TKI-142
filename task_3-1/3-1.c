d8509e0a41b2983d1fb3428d359133d0adb249d9
FedZik <kostikshabin2005@gmail.com>
2023-11-21 18:20:31 +0300

    fix

--- task_3-1/3-1.c
+++ task_3-1/3-1.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /**
 * @brief расситывает значение у
-* @param x значение х получаемое из цикла функции forFunc
+* @param x значение х получаемое из цикла for
 * @return возвращает значение у
 */
 double yFunc();
 
 /**
-* @brief табулирует значения х
+* @brief проверяет, существует ли функция в точке х
+* @param x аргумент функции
+* @return возвращает true если существует
 */
-void forFunc();
+bool isExists(const double x);
 
 /**
 * @brief проверка введеного значения
@@ -21,12 +24,44 @@ void forFunc();
 double getNumber();
 
 /**
+* @brief проверяет правильность ввода шага
+* @param step введенное значение шага расчета
+* @return возвращает true если ввод правильный
+*/
+bool checkStep(double step);
+
+/**
+* @brief проверяет правильность ввода начальной и конечной точек расчета
+* @param startPoint введенное значение начальной точки расчета
+* @param endPoint введенное значение конечной точки расчета
+* @return возвращает true если ввод правильный
+*/
+bool comparePoints(double startPoint, double endPoint);
+
+/**
 * @brief Точка входа в программу
 * @return Возвращает 0, если программа работает корректно, иначе 1
 */
 int main()
 {
-    forFunc();
+    double startPoint = getNumber();
+    double endPoint = getNumber();
+    double step = getNumber();
+    if(checkStep(step) && comparePoints(startPoint, endPoint))
+    {
+        for(double x = startPoint; x <= endPoint; x += step)  
+        {
+            if (isExists(x))
+            {
+                const double y = yFunc(x);
+                printf("\n %7.4lf %7.4lf", x, y);
+            }
+            else
+            {
+                printf("\n %7.4lf %s", x, "no solution");
+            }
+        }
+    }
     return 0;
 }
 
@@ -35,32 +70,48 @@ double yFunc(double x)
     return 3 * sin(sqrt(x)) + 0.39 * x - 3.8;
 }
 
-void forFunc()
-{
-    double startPoint = getNumber();
-    double endPoint = getNumber();
-    double step = getNumber();
-    if (endPoint < startPoint)
-    {
-        printf("%s" "Wrong value"); 
-        abort();
-    }
-    else
-    {
-        for(double x = startPoint; x <= endPoint; x += step)  
-        {
-        printf ("\n  %9lf", yFunc(x));
-        }
-    }
-}
+
 
 double getNumber() 
 { 
     double number; 
     if (scanf("%lf", &number) != 1) 
     { 
-         printf("%s" "Wrong value"); 
+         errno = EIO; 
+         perror("Invalid value entered");  
          abort();  
     } 
     return number;
+}
+
+bool isExists(const double x)
+{
+    if (x > __DBL_EPSILON__)
+    {
+        return true;
+    }
+    return false;
+    
+}
+
+bool checkStep(double step)
+{
+    if (step <= __DBL_EPSILON__)
+    {
+        errno = EIO; 
+        perror("Invalid value entered"); 
+        abort();
+    }
+    return true;
+}
+
+bool comparePoints(double startPoint, double endPoint)
+{
+    if (endPoint <= startPoint)
+    {
+        errno = EIO; 
+        perror("Invalid value entered"); 
+        abort();
+    }
+    return true;
 }
