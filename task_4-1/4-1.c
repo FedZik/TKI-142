beed25a7bcac4f4aa6df28caaf2717e8d12835f1
FedZik <kostikshabin2005@gmail.com>
2023-12-07 23:28:54 +0300

    fix
    +документация

--- task_4-1/4-1.c
+++ task_4-1/4-1.c
@@ -4,104 +4,124 @@
 #include <errno.h>
 
 /**
- * @brief Функция присваивает целочисленное значение переменной
+ * @brief присваивает целочисленное значение переменной
  * @return целочисленная цифра
 */
-int get_int();
+int getInt();
 
 /**
- * @brief Функция присваивает переменной целочисленное значение и проверяет его на положителность
+ * @brief присваивает переменной целочисленное значение и проверяет его на положителность
  * @return number положительное число
 */
-size_t get_size_t();
+size_t getSizeT();
 
 /**
- * @brief структура хранит константы, указывающие выбор пользователя заполнить массив
- * @brief user_choice хранит значение, вызывающее ввод массива вручную
- * @brief random_choice хранит значение, вызывающее ввод массива случайными числами
+ * @brief хранит константы, указывающие выбор пользователя для заполнения массива
+ * @brief userChoice хранит значение, вызывающее ввод массива вручную
+ * @brief randomChoice хранит значение, вызывающее ввод массива случайными числами
 */
 enum Choices
 {
-    user_choice = 1,
-    random_choice = 2,
+    userChoice = 1,
+    randomChoice = 2,
 };
 
 /**
- * @brief Функция выделяет память под массив
+ * @brief выделяет память под массив
  * @param length длина массива
  * @return array указатель на пустой массив
 */
-int* get_mem_array(const int length);
+int* getMemoryArray(const int length);
 
 /**
- * @brief Функция заполняет массив, элементы вводит пользователь
+ * @brief заполняет массив, элементы вводит пользователь
  * @param array указатель на заполняемый массив
  * @param length длина массива
 */
-void user_array(int* array, const size_t length);
+void userArray(int* array, const size_t length);
 
 /**
- * @brief Функция заполняет массив рандомными числами в диапазоне [-100:200]
+ * @brief заполняет массив случайными числами в диапазоне [-10:10]
  * @param array указатель на заполняемый массив
  * @param length длина массива
 */
-void random_array(int* array, const size_t length);
+void randomArray(int* array, const size_t length);
 
 /**
- * @brief Функция, освобождающая массив
+ * @brief освобождение массива
  * @param array указатель на массив
 */
-void free_array(int* array);
+void freeArray(int* array);
 
-int first_task(int* array, size_t length);
+/**
+ * @brief находит сумму элементов, имеющих нечетное значение
+ * @param array указатель на заполняемый массив
+ * @param length длина массива
+*/
+int firstTask(int* array, size_t length);
 
-void second_task(int* array, size_t length, int number);
+/**
+ * @brief выводит индексы элементов, значения которых больше заданного числа
+ * @param array указатель на заполняемый массив
+ * @param length длина массива
+ * @param number заданное число
+*/
+void secondTask(int* array, size_t length, int number);
 
-void third_task(int* array, const int length);
+/**
+ * @brief заменяет второй элемент массива на максимальный среди отрицательных и выводит его
+ * @param array указатель на заполняемый массив
+ * @param length длина массива
+*/
+void thirdTask(int* array, const int length);
 
+/**
+ * @brief находит наибольшее отрицательное значение в массиве
+ * @param length длина массива
+ * @return array указатель на пустой массив
+*/
 int maxNegative (int* array, const int length);
 
 /**
  * @brief Точка входа в программу
- * @return 0 Программа исправна
- * @return 1 Программа досрочно закрылась с ошибкой
+ * @return Возвращает 0 если программа работет исправно, иначе 1
 */
 int main()
 {
     printf("insert a length of array \n");
-    size_t length = get_size_t();
-    printf("enter %d if you want to fill array by youself or %d to fill array by random numbers \n", user_choice, random_choice);
-    int choice = get_int();
-    int mas = get_mem_array(length);
+    size_t length = getSizeT();
+    printf("enter %d if you want to fill array by youself or %d to fill array by random numbers \n", userChoice, randomChoice);
+    int choice = getInt();
+    int mas = getMemoryArray(length);
     switch ("%d", choice)
     {
-        case random_choice:
-            random_array(mas, length);
+        case randomChoice:
+            randomArray(mas, length);
             break;
-        case user_choice:
-            user_array(mas, length);
+        case userChoice:
+            userArray(mas, length);
             break;
         default:
             puts("Entered invalid number!\n");
             return 1;
     }
     printf("enter an integer number \n");
-    int number = get_int();
-    printf("First task: %d\n", first_task(mas, length));
-    second_task(mas, length, number);
+    int number = getInt();
+    printf("First task: %d\n", firstTask(mas, length));
+    secondTask(mas, length, number);
     if (maxNegative(mas, length) == INT_MIN)
     {
         printf("\nThird task: Not found\n");
     }
     else
     {
-        third_task(mas, length);
+        thirdTask(mas, length);
     }
-    free_array(mas);
+    freeArray(mas);
     return 0;
 }
 
-int get_int()
+int getInt()
 {
     int number;
     if (scanf("%d", &number) != 1)
@@ -112,9 +132,9 @@ int get_int()
     return number;
 }
 
-size_t get_size_t()
+size_t getSizeT()
 {
-    int number = get_int();
+    int number = getInt();
     if (number <= 0)
     {
         errno = ERANGE;
@@ -124,7 +144,7 @@ size_t get_size_t()
     return (size_t)number;
 }
 
-int* get_mem_array(const int length)
+int* getMemoryArray(const int length)
 {
     int* array = malloc(length * sizeof(int));
     if (NULL == array)
@@ -136,25 +156,25 @@ int* get_mem_array(const int length)
     return array;
 }
 
-void user_array(int* array, const size_t length)
+void userArray(int* array, const size_t length)
 {
     puts("Insert array elements:");
 
     for (size_t i = 0; i < length; i++)
     {
-        array[i] = get_int();
+        array[i] = getInt();
     }
 }
 
-void random_array(int* array, const size_t length)
+void randomArray(int* array, const size_t length)
 {
     for (size_t i = 0; i < length; i++)
     {
-        array[i] = rand() % 20 - 10;
+        array[i] = rand() % 10 - 10;
     }
 }
 
-void free_array(int* array)
+void freeArray(int* array)
 {
     if (NULL != array)
     {
@@ -162,7 +182,7 @@ void free_array(int* array)
     }
 }
 
-int first_task(int* array, size_t length)
+int firstTask(int* array, size_t length)
 {
     int sum = 0;
     for (size_t i = 0; i < length; i++)
@@ -175,7 +195,7 @@ int first_task(int* array, size_t length)
     return sum;
 }
 
-void second_task(int* array, size_t length, int number)
+void secondTask(int* array, size_t length, int number)
 {
     printf("Second task: ");
     for (size_t i = 0; i <= length; i++)
@@ -187,7 +207,7 @@ void second_task(int* array, size_t length, int number)
     }
 }
 
-void third_task(int* array, const int length)
+void thirdTask(int* array, const int length)
 {
     array[1] = maxNegative(array, length);
     printf("\nThird task: %d", array[1]);
