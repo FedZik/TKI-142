6704fce800dd5d3cb1a1491869e28db02f9e962e
FedZik <kostikshabin2005@gmail.com>
2023-12-20 21:46:57 +0300

    rework
    delete 3-2b

--- task_3-2/3-2.c
+++ task_3-2/3-2.c
@@ -1,78 +1,111 @@
-#include <stdio.h> 
-#include <errno.h> 
-#include <stdlib.h> 
- 
-/** 
-* @brief Функция считывающая количество членов последовательности
-* @return Количество членов последовательности
-*/ 
-int getCount(); 
- 
-/** 
-* @brief Функция расчитывающая сумму членов последовательности
-* @param count - количество членов последовательности
-* @return Сумма последовательности
-*/ 
-double getSum(int count); 
- 
-/** 
-* @brief Функция расчитывающая рекурентный член последовательности 
-* @param k - номер члена последовательности
-* @return Значение рекурентного члена последовательности
-*/ 
-double getRecurrent(int k); 
+#include <stdio.h>
+#include <float.h>
+#include <errno.h>
+#include <stdlib.h>
 
-/** 
-* @brief Функция ищет первое значение реккурентной формулы
-* @return Значение первого члена реккурентной формулы
-*/ 
-double findFirst();
+/**
+* @brief Функция считывающая количество членов последовательности.
+* @remarks Экстренное завершение программы, в случае неправильного ввода.
+* @return Количество членов последовательности.
+*/
+int getCount();
 
+/**
+* @brief Функция расчитывающая сумму членов последовательности.
+* @param count - количество членов последовательности.
+* @return Сумма последовательности.
+*/
+double getFirstSum(int count);
 
-/** 
-* @brief Точка входа в программу
-* @return Возвращает 0 в случае успеха
-*/ 
-int main() 
-{ 
-        printf("%s", "Insert the end of the segment "); 
-        int count = getCount(); 
-        printf("First task: %.15lf \n", getSum(count));
-        return 0; 
-} 
+/**
+* @brief Функция расчитывающая рекурентный член последовательности.
+* @param k номер члена последовательности.
+* @return Значение рекурентного члена последовательности.
+*/
+double getReccurent(int k);
 
-int getCount() 
-{ 
-        int count; 
-        int result = scanf("%d", &count); 
-        if (result != 1 || count < 0) 
-        { 
-                errno = EIO; 
-                perror("Error:"); 
-                abort(); 
-        } 
-        return count; 
-} 
- 
-double getSum(int count) 
-{ 
-        double current = findFirst();
-        double sum = current; 
-        for (int k = 1; k < count; k++) 
-        { 
-                current *= getRecurrent(k); 
-                sum += current; 
-        } 
-        return sum; 
-} 
- 
-double getRecurrent(int k) 
-{ 
-        return 1.0 / ((k + 3) * (k + 4)); 
+/**
+* @brief Функция ввода и проверки на правильность точности вычислений.
+* @return Точность вычислений.
+*/
+double getEpsilon();
+
+/**
+* @brief Функция вычисления суммы членов последовательности, не меньших e.
+* @param e - точность вычислений.
+* @return Сумма.
+*/
+double getSecondSum(const double e);
+
+/**
+* @brief Точка входа в программу.
+* @return Возвращает 0 в случае успеха.
+*/
+int main()
+{
+    printf("%s", "Enter the end of the sequence: ");
+    int count = getCount();
+    printf("Sum of %d terms of the sequence: %.15lf \n", count, getFirstSum(count));
+    printf("Enter 'e': ");
+    const double e = getEpsilon();
+    printf("The sum of the terms of the sequence which is not less than the accuracy: %.15lf\n", getSecondSum(e));
+    return 0;
 }
 
-double findFirst()
+int getCount()
 {
-        double k = 0;
-        return 1.0 / ((k + 3) * (k + 4)); 
+	int count;
+	int result = scanf("%d", &count);
+	if (result != 1 || count < 0)
+	{
+		errno = EIO;
+		perror("Error");
+		abort();
+	}
+	return count;
+}
+
+double getFirstSum(int count)
+{
+	double current = getReccurent(0);
+	double sum = current;
+	for (int k = 1; k <= count; k++)
+	{
+		current *= getReccurent(k);
+		sum += current;
+	}
+	return sum;
+}
+
+double getReccurent(int k)
+{
+  return 1.0 / ((k + 3) * (k + 4));
+	
+}
+
+double getEpsilon()
+{
+	double e;
+	double result = scanf("%lf", &e);
+	if (result != 1 || e < DBL_EPSILON)
+	{
+		errno = EIO;
+		perror("Error");
+		abort();
+	}
+	return e;
+}
+
+double getSecondSum(const double e)
+{
+	double current = getReccurent(0);
+	double sum = current;
+	int k = 1;
+	while (current >= e)
+	{
+		current *= getReccurent(k);
+		sum += current;
+		k++;
+	}
+	return sum;
 }
